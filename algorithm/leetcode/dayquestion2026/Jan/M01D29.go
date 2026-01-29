8580c7b8b23dd13b0a93490e08c9cc82a1b60b1e
logos <2378884181@qq.com>
2026-01-29 19:35:25 +0800

    feat(leetcode): 实现转换字符串的最小成本算法
    - 新增函数 minimumCost 实现字符串转换最小成本计算
    - 使用 Floyd 算法计算字符间的最短转换路径
    - 处理无法转换的情况返回 -1
    - 输入包括原始和目标字符串以及转换代价数组
    - 代码中定义了无穷大值初始化距离矩阵
    - 结果累加所有字符的最小转换代价后返回

---
+++ algorithm/leetcode/dayquestion2026/Jan/M01D29.go
@@ -0,0 +1,44 @@
+package Jan
+
+import "math"
+
+/**
+ * 2976. 转换字符串的最小成本 I
+ * <a href="https://leetcode.cn/problems/minimum-cost-to-convert-string-i/description/"/>
+ */
+
+func minimumCost(source, target string, original, changed []byte, cost []int) (ans int64) {
+	const inf = math.MaxInt / 2
+	dis := [26][26]int{}
+	for i := range dis {
+		for j := range dis[i] {
+			if j != i {
+				dis[i][j] = inf
+			}
+		}
+	}
+	for i, c := range cost {
+		x := original[i] - 'a'
+		y := changed[i] - 'a'
+		dis[x][y] = min(dis[x][y], c)
+	}
+	for k := range dis {
+		for i := range dis {
+			if dis[i][k] == inf {
+				continue
+			}
+			for j := range dis {
+				dis[i][j] = min(dis[i][j], dis[i][k]+dis[k][j])
+			}
+		}
+	}
+
+	for i, b := range source {
+		d := dis[b-'a'][target[i]-'a']
+		if d == inf {
+			return -1
+		}
+		ans += int64(d)
+	}
+	return
+}
