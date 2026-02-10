df540fde4c6cec241ffb7b0dbb6c7fe183e2a9ce
logos <2378884181@qq.com>
2026-02-10 12:55:04 +0800

    feat(Feb): 添加最长平衡子数组算法实现
    - 新增3719题最长平衡子数组I的解决方案
    - 采用区块分块和前缀和思想提升查询效率
    - 实现懒惰标记优化区间加法操作
    - 通过哈希映射维护元素出现位置
    - 使用数学技巧判断平衡子数组最大长度

---
+++ algorithm/leetcode/dayquestion2026/Feb/M02D10.go
@@ -0,0 +1,91 @@
+package Feb
+
+import "math"
+
+/**
+ * 3719. 最长平衡子数组 I
+ * <a href="https://leetcode.cn/problems/longest-balanced-subarray-i/description/"/>
+ */
+
+func longestBalanced(nums []int) (ans int) {
+	n := len(nums)
+	B := int(math.Sqrt(float64(n+1)))/2 + 1
+	sum := make([]int, n+1)
+
+	type block struct {
+		l, r int
+		todo int
+		pos  map[int]int
+	}
+	blocks := make([]block, n/B+1)
+	calcPos := func(l, r int) map[int]int {
+		pos := map[int]int{}
+		for j := r - 1; j >= l; j-- {
+			pos[sum[j]] = j
+		}
+		return pos
+	}
+	for i := 0; i <= n; i += B {
+		r := min(i+B, n+1)
+		pos := calcPos(i, r)
+		blocks[i/B] = block{i, r, 0, pos}
+	}
+
+	rangeAdd := func(l, r, v int) {
+		for i := range blocks {
+			b := &blocks[i]
+			if b.r <= l {
+				continue
+			}
+			if b.l >= r {
+				break
+			}
+			if l <= b.l && b.r <= r {
+				b.todo += v
+			} else {
+				for j := b.l; j < b.r; j++ {
+					sum[j] += b.todo
+					if l <= j && j < r {
+						sum[j] += v
+					}
+				}
+				b.pos = calcPos(b.l, b.r)
+				b.todo = 0
+			}
+		}
+	}
+
+	findFirst := func(r, v int) int {
+		for i := range blocks {
+			b := &blocks[i]
+			if b.r <= r {
+				if j, ok := b.pos[v-b.todo]; ok {
+					return j
+				}
+			} else {
+				for j := b.l; j < r; j++ {
+					if sum[j] == v-b.todo {
+						return j
+					}
+				}
+				break
+			}
+		}
+		return n
+	}
+	last := map[int]int{}
+	for i := 1; i <= n; i++ {
+		x := nums[i-1]
+		v := x%2*2 - 1
+		if j := last[x]; j == 0 {
+			rangeAdd(i, n+1, v)
+		} else {
+			rangeAdd(j, i, -v)
+		}
+		last[x] = i
+
+		s := sum[i] + blocks[i/B].todo
+		ans = max(ans, i-findFirst(i-ans, s))
+	}
+	return
+}
